647367241930bbed7054d45ff47453a3c936e077
jon-fearer <fearer.jon@gmail.com>
2018-12-22 20:51:36 -0700

    Use postgres docker

--- news_app/static/sql/dml.py
+++ news_app/static/sql/dml.py
@@ -9,7 +9,10 @@ f = d['features']
 sql = '''INSERT INTO news_app_region (name, created_on, updated_on,
          region_category_id, census_area, geo_id, state_number)
          VALUES (%s, %s, %s, %s, %s, %s, %s);'''
-cnxn = psycopg2.connect(dbname='.', user='.', password='.', host='.')
+cnxn = psycopg2.connect(dbname='news_app',
+                        user='postgres',
+                        password='mysecretpassword',
+                        host='localhost')
 crsr = cnxn.cursor()
 for feature in f:
     crsr.execute(sql, [feature['properties']['NAME'],
@@ -27,7 +30,10 @@ f = d['features']
 sql = '''INSERT INTO news_app_region (name, created_on, updated_on,
          region_category_id, census_area, geo_id, state_number)
          VALUES (%s, %s, %s, %s, %s, %s, %s);'''
-cnxn = psycopg2.connect(dbname='.', user='.', password='.', host='.')
+cnxn = psycopg2.connect(dbname='news_app',
+                        user='postgres',
+                        password='mysecretpassword',
+                        host='localhost')
 crsr = cnxn.cursor()
 for feature in f:
     crsr.execute(sql, [feature['properties']['name'],
--- news_app/static/sql/dml.sql
+++ news_app/static/sql/dml.sql
@@ -1,10 +1,10 @@
-INSERT INTO news_app_regioncategory (name, created_on, updated_on)
+INSERT INTO news_app_region_category (name, created_on, updated_on)
 VALUES
 ('State', now(), now()),
 ('Province', now(), now()),
 ('Country', now(), now()),
 ('Continent', now(), now());
 
-INSERT INTO news_app_publishingorganization (name, abbreviation, created_on, updated_on)
+INSERT INTO news_app_publishing_organization (name, abbreviation, created_on, updated_on)
 VALUES
 ('Cable News Network', 'CNN', now(), now());
